ae8f4755248d56a5f590f45dd363a8b4ce0d79f6
Daniel <danielptm@me.com>
2018-06-26 22:34:57 +0200

    Prepared problem4

---
+++ app/test/ProblemFour.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+import ModuleFour from '../src/modules/ModuleFour';
+
+describe('Coding practice problems: https://projecteuler.net/problem=4',() => {
+  describe('\n A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.', () => {
+    it('Check to make sure that a palindrome is calculated correctly.', () => {
+      console.log('NOT IMPLEMENTED');
+    });
+  });
+});
