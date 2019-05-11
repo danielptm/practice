36ff6fb49796116052be18eb1a3914f1d1c3164f
Daniel <danielptm@me.com>
2019-05-11 16:14:10 -0700

    Improved the answer to the twoSum problem to include exception handling.

--- java/src/main/java/practice/leetcode/LeetCode.java
+++ java/src/main/java/practice/leetcode/LeetCode.java
@@ -11,15 +11,18 @@ public class LeetCode {
      * @return
      */
     public static int[] twoSum(int[] nums, int target) {
-        int[] intsToReturn = new int[2];
+        int[] intsToReturn = new int[]{-1, -1};
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    intsToReturn[0] = i;
-                    intsToReturn[1] = j;
+                    intsToReturn[0] = j;
+                    intsToReturn[1] = i;
                 }
             }
+        }
+        if (intsToReturn[0] == -1 && intsToReturn[1] == -1) {
+            throw new IllegalArgumentException("It is not possible to sum elements in the array to equal the target.");
         }
         return intsToReturn;
     }
--- java/src/test/java/practice/leetcode/TestLeetCode.java
+++ java/src/test/java/practice/leetcode/TestLeetCode.java
@@ -15,6 +15,20 @@ public class TestLeetCode {
         int[] items =  LeetCode.twoSum(given, target);
 
         Assert.assertEquals(9, given[items[0]] + given[items[1]]);
+
+
+        int target2 = 26;
+        int[] items2 =  LeetCode.twoSum(given, target2);
+
+        Assert.assertEquals(target2, given[items2[0]] + given[items2[1]]);
+
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void twoSumThrowException() {
+        int[] given = new int[]{2, 7, 11, 15};
+
+        int[] items = LeetCode.twoSum(given, 137);
     }
 
 
