d7c339433d9f7746619f01fe8991ae5f496e1660
Daniel <danielptm@me.com>
2019-09-07 16:29:28 -0700

    Added beginning for minimum swaps problem but it is not done

---
+++ java/src/main/java/hackerrank/MinimumSwaps.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+public class MinimumSwaps {
+
+    public static int minimumSwaps(int[] arr) {
+        int swaps = 0;
+        //Swap operation:
+
+        return swaps;
+    }
+
+
+}
---
+++ java/src/test/java/hackerrank/MinimumSwapsTest.java
@@ -0,0 +1,12 @@
+package hackerrank;
+
+import org.junit.Test;
+
+public class MinimumSwapsTest {
+
+    @Test
+    public void test() {
+        int[] input = new int[]{7,1,3,2,4,5,6};
+
+    }
+}
