32c5bca30b03b2d2d3a239bd319c680b16042ad5
Daniel <danielptm@me.com>
2020-01-16 22:24:40 -0800

    JAVA: Added a solution for the height of the binary tree problem.

---
+++ java/src/main/java/hackerrank/HeightOfBinaryTree.java
@@ -0,0 +1,76 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeightOfBinaryTree {
+
+
+    public static int height(Node root) {
+        List<List<Node>> listOfNodes = new ArrayList<>();
+        listOfNodes.add(new ArrayList<>());
+        listOfNodes.get(0).add(root);
+        int nodeListSize = listOfNodes.size();
+        for (int i = 0; i < nodeListSize; i++) {
+            int innerArraySize = listOfNodes.get(i).size();
+            for(int j = 0; j < innerArraySize; j++) {
+                int nextLevel = i + 2;
+                int indexForNextLevel = i + 1;
+                boolean nextLevelExists = nodeListSize == nextLevel ? true : false;
+                Node currLeft = listOfNodes.get(i).get(j).left;
+                Node currRight = listOfNodes.get(i).get(j).right;
+                List<Node> newList = new ArrayList<>();
+                if (currLeft!=null) {
+                    if (nextLevelExists) {
+                        listOfNodes.get(indexForNextLevel).add(currLeft);
+                    } else {
+                        newList.add(currLeft);
+                    }
+                }
+                if (currRight!=null) {
+                    if (nextLevelExists) {
+                        listOfNodes.get(indexForNextLevel).add(currRight);
+                    } else {
+                        newList.add(currRight);
+                    }
+                }
+                if (!newList.isEmpty()) {
+                    listOfNodes.add(newList);
+                }
+                nodeListSize = listOfNodes.size();
+            }
+        }
+        return listOfNodes.size() - 1;
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+}
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    boolean isChecked;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+        isChecked = false;
+    }
+}
---
+++ java/src/test/java/hackerrank/HeightOfBinaryTreeTest.java
@@ -0,0 +1,32 @@
+package hackerrank;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HeightOfBinaryTreeTest {
+
+
+    @Test
+    public void testing() {
+        Node node = new Node(3);
+        HeightOfBinaryTree.insert(node, 2);
+        HeightOfBinaryTree.insert(node, 1);
+        HeightOfBinaryTree.insert(node, 5);
+        HeightOfBinaryTree.insert(node, 4);
+        HeightOfBinaryTree.insert(node, 6);
+        HeightOfBinaryTree.insert(node, 7);
+
+        int result = HeightOfBinaryTree.height(node);
+
+        Assert.assertEquals(3, result);
+
+    }
+
+    @Test
+    public void testing2() {
+        int nodeListSize = 3;
+        int nextLevel = 1 + 2;
+        boolean levelUpExists = nodeListSize == nextLevel ? true : false;
+        System.out.println(levelUpExists);
+    }
+}
