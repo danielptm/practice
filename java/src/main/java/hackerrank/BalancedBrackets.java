ed97316f434bd3258c93c149d92b1beb1bf46de7
Daniel <danielptm@me.com>
2020-02-28 14:51:04 -0800

    Removed theold balanced brackets code

--- java/src/main/java/hackerrank/BalancedBrackets.java
+++ java/src/main/java/hackerrank/BalancedBrackets.java
@@ -13,48 +13,7 @@ import java.util.Map;
 public class BalancedBrackets {
     // Complete the isBalanced function below.
     static String isBalanced(String s) {
-        Map<String, Map<String, String>> items = new HashMap<>();
-
-        Map<String, String> curvy = new HashMap<>();
-        curvy.put("left", "{");
-        curvy.put("right", "}");
-        items.put("{", curvy);
-
-        Map<String, String> bracket = new HashMap<>();
-        bracket.put("left", "[");
-        bracket.put("right", "]");
-        items.put("[", bracket);
-
-        Map<String, String> parenthesis = new HashMap<>();
-        parenthesis.put("left", "(");
-        parenthesis.put("right", ")");
-        items.put("(", parenthesis);
-
-
-        Map<String, String> curvy2 = new HashMap<>();
-        curvy2.put("left", "{");
-        curvy2.put("right", "}");
-        items.put("}", curvy2);
-
-        Map<String, String> bracket2 = new HashMap<>();
-        bracket2.put("left", "[");
-        bracket2.put("right", "]");
-        items.put("]", bracket2);
-
-        Map<String, String> parenthesis2 = new HashMap<>();
-        parenthesis2.put("left", "(");
-        parenthesis2.put("right", ")");
-        items.put(")", parenthesis2);
-
-        String result = "NO";
-        int length = s.length();
-
-        if ( (length % 2) != 0) {
-            return result;
-        } else {
-
-        }
-        return result;
+        return null;
     }
 
 }
