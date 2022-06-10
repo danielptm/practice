179ab0c1d3ba4fcb6ae7b7071e676f2b4b46691e
Daniel <danielptm@me.com>
2022-06-10 16:07:04 -0700

    retrigger

--- java/src/main/java/leetcode/studyplans/programmingskills1/ProgrammingSkills1.java
+++ java/src/main/java/leetcode/studyplans/programmingskills1/ProgrammingSkills1.java
@@ -1,5 +1,13 @@
 package leetcode.studyplans.programmingskills1;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProgrammingSkills1 {
 
     public int countOdds(int low, int high) {
@@ -13,5 +21,22 @@ public class ProgrammingSkills1 {
             }
         }
         return - 1;
+    }
+
+    public Double average(int[] s) {
+
+        List<Double> items = Arrays.stream(s).mapToDouble(i -> (double) i).boxed()
+                .collect(Collectors.toList());
+        Collections.sort(items);
+
+        Double max = items.get(items.size() - 1);
+        Double min = items.get(0);
+        Double res = items.stream()
+                .filter(i -> i != max && i != min)
+                .reduce((i,j) -> i + j)
+                .get();
+        res = (res / (items.size() - 2));
+        String sres = String.format("%.5f", res);
+        return Double.valueOf(sres);
     }
 }
--- java/src/test/java/leetcode/studyplans/programmingskills1/ProgrammingSkills1Test.java
+++ java/src/test/java/leetcode/studyplans/programmingskills1/ProgrammingSkills1Test.java
@@ -36,4 +36,21 @@ public class ProgrammingSkills1Test {
         int res = new ProgrammingSkills1().countOdds(l, h);
         Assert.assertEquals(1, res);
     }
+
+    @Test
+    public void testAverage() {
+        int[] salary = new int[]{4000,3000,1000,2000};
+        double expected = 2500.00000;
+        double res = new ProgrammingSkills1().average(salary);
+        Assert.assertTrue(expected == res);
+    }
+
+    @Test
+    public void testAverage2() {
+        int[] salary = new int[]{48000,59000,99000,13000,78000,45000,31000,17000,39000,37000,93000,77000,33000,28000,4000,54000,67000,6000,1000,11000};
+        double expected = 41111.11111;
+        double res = new ProgrammingSkills1().average(salary);
+        Assert.assertTrue(expected == res);
+    }
+
 }
