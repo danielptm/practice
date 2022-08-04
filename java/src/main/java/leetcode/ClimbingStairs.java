35196377cb0714c111f5e1f65742880ce40eb888
Daniel Tuttle <danieltuttle@Daniels-Mac-Studio.local>
2022-08-04 15:02:28 -0700

    alt, doesnt work

--- java/src/main/java/leetcode/ClimbingStairs.java
+++ java/src/main/java/leetcode/ClimbingStairs.java
@@ -1,10 +1,16 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 // https://leetcode.com/problems/climbing-stairs/
+//TODO
+/**
+ * Do this with a hashmap and a list. With the hashmap store the key as a generated path.
+ * The value is an object. The object has a boolean saying if equals n or not.
+ * Use the list to store the most recent generated paths that have been stored in the hashmap.
+ * Retrieve these most recent ones and get each object from the map, create a new path, value, store it in list, repeat
+ */
 public class ClimbingStairs {
     public int climbStairs(int n) {
         if (n == 1) {
@@ -14,37 +20,102 @@ public class ClimbingStairs {
             return 0;
         }
 
-        List<String> paths = new ArrayList<>();
         String leastEfficientPath = generateLeastEfficientPath(n);
+        List<String> mostEfficientPaths = generateMostEfficientPaths(n);
+
+        Map<String, Path> pathMap = new HashMap<>();
+        List<String> paths = new ArrayList<>();
+
+        for (String mostEfficientPath: mostEfficientPaths) {
+            pathMap.put(mostEfficientPath, new Path(mostEfficientPath, n));
+            paths.add(mostEfficientPath);
+        }
+
 
         while (!leastEfficientPathExists(paths, leastEfficientPath)) {
-            if (paths.isEmpty()) {
-                paths.add("1");
-                paths.add("2");
-                continue;
-            }
             List<String> newPaths = new ArrayList<>();
-            //TODO
-            /**
-             * Do this with a hashmap and a list. With the hashmap store the key as a generated path.
-             * The value is an object. The object has a boolean saying if equals n or not.
-             * Use the list to store the most recent generated paths that have been stored in the hashmap.
-             * Retrieve these most recent ones and get each object from the map, create a new path, value, store it in list, repeat
-             */
             for (int i = 0; i < paths.size(); i++) {
                 String np1 = paths.get(i) + ":1";
                 String np2 = paths.get(i) + ":2";
-                if (!stringExists(paths, np1)) {
+                if (!stringExists(newPaths, np1) && !stringExists(paths, np1)) {
+                    Path path = new Path(np1, n);
+                    pathMap.put(np1, path);
                     newPaths.add(np1);
                 }
-                if (!stringExists(paths, np2)) {
+                if (!stringExists(newPaths, np2) && !stringExists(paths, np2)) {
+                    Path path = new Path(np2, n);
+                    pathMap.put(np2, path);
                     newPaths.add(np2);
+
                 }
             }
+            paths = new ArrayList<>();
             paths.addAll(newPaths);
         }
-        int res = calculateNumberPaths(paths, n);
+        int res =  sumPaths(pathMap);
         return res;
+    }
+
+     List<String> generateMostEfficientPaths(int n) {
+        String p = "";
+        int dividedTwo = n / 2;
+        List<String> mePaths = new ArrayList<>();
+        if (dividedTwo == 1) {
+            mePaths.add("1");
+            mePaths.add("2");
+            return mePaths;
+        }
+        if (n % 2 == 0) {
+            StringJoiner sb = new StringJoiner(":", "", "");
+            for (int i = 0; i < dividedTwo; i++) {
+                sb.add("2");
+            }
+            p = sb.toString();
+            String left = new String(p);
+            String right = new String(p);
+            left = left.substring(2);
+            right = left.substring(0, right.length() - 2);
+            left = "1:" + left;
+            right = right + ":1";
+            String opp = p.replace("2", "1");
+            mePaths.add(p);
+            mePaths.add(right);
+            mePaths.add(left);
+            mePaths.add(opp);
+        } else {
+            // This has the capacity to cause a bug later down the road, because there are 2 equally efficient paths.
+            StringJoiner sb = new StringJoiner(":", "", "");
+            sb.add("1");
+            for (int i = 0; i < dividedTwo; i++) {
+                sb.add("2");
+                p = sb.toString();
+                mePaths.add(p);
+            }
+            StringJoiner sb2 = new StringJoiner(":", "", "");
+            for (int i = 0; i < dividedTwo; i++) {
+                sb2.add("2");
+            }
+            sb2.add("1");
+            p = sb2.toString();
+            String opp = p.replace("2", "1");
+            mePaths.add(opp);
+            mePaths.add(p);
+        }
+        return mePaths;
+    }
+
+    List<String> generateMostEfficientOpposite(List<String> n) {
+        return n.stream().map(s -> s.replace("2", "1")).collect(Collectors.toList());
+    }
+
+    int sumPaths(Map<String, Path> pathMap) {
+        int count = 0;
+        for(Map.Entry<String, Path> m: pathMap.entrySet()) {
+            if (m.getValue().isEqualsN()) {
+                count += 1;
+            }
+        }
+        return count;
     }
 
     String generateLeastEfficientPath(int n ) {
@@ -56,17 +127,17 @@ public class ClimbingStairs {
         return sb.toString();
     }
 
-    int calculateNumberPaths(List<String> paths, int n) {
-        int count = 0;
-        for (String p: paths) {
-            String[] pathBits = p.split(":");
-            int sum = sumPaths(pathBits);
-            if (sum == n) {
-                count += 1;
-            }
-        }
-        return count;
-    }
+//    int calculateNumberPaths(List<String> paths, int n) {
+//        int count = 0;
+//        for (String p: paths) {
+//            String[] pathBits = p.split(":");
+//            int sum = sumPaths(pathBits);
+//            if (sum == n) {
+//                count += 1;
+//            }
+//        }
+//        return count;
+//    }
 
     int sumPaths(String[] pathBits) {
         int sum = 0;
@@ -84,5 +155,36 @@ public class ClimbingStairs {
     boolean leastEfficientPathExists(List<String> list, String s) {
         boolean res = list.stream().filter(i -> i.equals(s)).findFirst().isPresent();
         return res;
+    }
+}
+
+class Path {
+    int n;
+    private int sum = 0;
+
+    public Path(String path, int n) {
+        this.n = n;
+        this.sum = sum(path, n);
+    }
+
+    public boolean isEqualsN() {
+        return this.sum == this.n;
+    }
+
+    public int getSum() {
+        return this.sum;
+    }
+
+    private int sum(String path, int n) {
+        int sum = 0;
+        try {
+            String[] pathBits = path.split(":");
+            for (int i = 0; i < pathBits.length; i++) {
+                sum += Integer.valueOf(pathBits[i]);
+            }
+        } catch (Exception e) {
+            System.err.println("There was a problem creating the Path");
+        }
+        return sum;
     }
 }
--- java/src/test/java/leetcode/ClimbingStairsTest.java
+++ java/src/test/java/leetcode/ClimbingStairsTest.java
@@ -23,29 +23,71 @@ public class ClimbingStairsTest {
     }
 
     @Test
+    public void climbStairsTest3() {
+        int n = 4;
+        int res = new ClimbingStairs().climbStairs(n);
+        Assert.assertEquals(5, res);
+    }
+
+    @Test
+    public void climbStairsTest4() {
+        int n = 5;
+        int res = new ClimbingStairs().climbStairs(n);
+        Assert.assertEquals(8, res);
+    }
+
+//    @Test
+//    public void climbStairsTest3() {
+//        int n = 35;
+//        int res = new ClimbingStairs().climbStairs(n);
+//        Assert.assertEquals(2, res);
+//    }
+
+    @Test
     public void testGenerate() {
         String res = new ClimbingStairs().generateLeastEfficientPath(3);
         Assert.assertEquals("1:1:1", res);
     }
 
 
-    @Test
-    public void testCalculatePaths() {
-        List<String> paths = new ArrayList<>();
-        paths.add("1:1:1");
-        paths.add("1:2");
-        paths.add("2:1");
-        paths.add("2:2:2:2");
-
-        int res = new ClimbingStairs().calculateNumberPaths(paths, 3);
-
-        Assert.assertEquals(3, res);
-    }
+//    @Test
+//    public void testCalculatePaths() {
+//        List<String> paths = new ArrayList<>();
+//        paths.add("1:1:1");
+//        paths.add("1:2");
+//        paths.add("2:1");
+//        paths.add("2:2:2:2");
+//
+//        int res = new ClimbingStairs().calculateNumberPaths(paths, 3);
+//
+//        Assert.assertEquals(3, res);
+//    }
 
     @Test
     public void testSumPaths() {
         String[] pathBits = new String[]{"1", "2"};
         int res = new ClimbingStairs().sumPaths(pathBits);
-        Assert.assertEquals(1, res);
+        Assert.assertEquals(3, res);
+    }
+
+
+    @Test
+    public void testPath() {
+        String path = "1:1";
+        Path p = new Path(path, 2);
+        boolean result = p.isEqualsN();
+        Assert.assertEquals(true, result);
+    }
+
+//    @Test
+//    public void testing() {
+//        System.out.println(3 / 2);
+//    }
+
+    @Test
+    public void generateMostEfficientPathTest() {
+        int n = 3;
+        List<String > res = new ClimbingStairs().generateMostEfficientPaths(n);
+        Assert.assertEquals(2, res.size());
     }
 }
