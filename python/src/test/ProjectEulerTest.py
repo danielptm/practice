9a734729f048e99e621a9d3beb5def956b384fcd
Daniel Tuttle <danieltuttle@danielsacstudio.mynetworksettings.com>
2023-10-13 08:44:04 -0700

    added projecteuler problems

---
+++ python/src/test/ProjectEulerTest.py
@@ -0,0 +1,9 @@
+import unittest
+
+from src.problems.ProjectEuler import ProjectEuler
+
+
+class ProjectEulerTest(unittest.TestCase)
+    def test_problem1(self):
+        pj = ProjectEuler()
+        res = pj.problem1()
