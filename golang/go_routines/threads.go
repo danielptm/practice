4052c969b7624cd4a485a4150fb34532e4e51119
Daniel Tuttle <danieltuttle@Daniels-MacBook-Pro-2.local>
2023-11-04 11:21:35 -0700

    added golang goroutine and channel example

---
+++ golang/go_routines/threads.go
@@ -0,0 +1,16 @@
+package go_routines
+
+import "time"
+
+func run1() {
+	println("before sleep")
+	time.Sleep(2000)
+	println("after sleep")
+}
+
+func run2(c chan string) {
+	println("before sleep")
+	time.Sleep(3000 * time.Millisecond)
+	println("after sleep")
+	c <- "from run2(c)"
+}
---
+++ golang/go_routines/threads_test.go
@@ -0,0 +1,17 @@
+package go_routines
+
+import "testing"
+
+// basic goroutine example
+func TestRun(t *testing.T) {
+	go run1()
+	println("In test, after call to run()")
+}
+
+// goroutine with channel and blocking
+func TestRun2(t *testing.T) {
+	c := make(chan string)
+	go run2(c)
+	println("In test, after call to run()")
+	println(<-c)
+}
