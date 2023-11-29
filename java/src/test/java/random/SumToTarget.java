9d03d0c41630a308810d4db72173db66a26a2892
Daniel Tuttle <danieltuttle@Daniels-Mac-Studio.local>
2023-11-29 10:55:39 -0800

    added java code

--- java/src/test/java/random/SumToTarget.java
+++ java/src/test/java/random/SumToTarget.java
@@ -1,2 +1,33 @@
-package random;public class SumToTarget {
+package random;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SumToTarget {
+
+
+    private int[] sumToTarget(int[] items, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items.length; j++) {
+                if ((items[i] + items[j]) == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void sumToTarget() {
+        int[] items = new int[]{1, 6, 2, 4, 8, 3};
+        int target = 5;
+        int[] result = sumToTarget(items, target);
+        Assert.assertEquals(2, result.length);
+        int[] expected = new int[]{5, 2};
+        Assert.assertTrue(Arrays.equals(expected, result));
+    }
 }
