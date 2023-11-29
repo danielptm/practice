4c5432cf893aa68df00d31742bada9aa547a0e4d
Daniel Tuttle <danieltuttle@Daniels-Mac-Studio.local>
2023-11-29 10:55:23 -0800

    addded java code

--- java/src/main/java/warmup/Warmup.java
+++ java/src/main/java/warmup/Warmup.java
@@ -1,2 +1,50 @@
-package warmup;public class Warmup {
+package warmup;
+
+public class Warmup {
+    public void StringsPractice(String text) {
+        print("String: " + text);
+        int index = 2;
+        print("Remove string at index: " + index);
+        String c = String.valueOf(text.charAt(index));
+        print(c);
+        print("Get the unicode code point at the index.");
+        int code = text.codePointAt(index);
+        print(String.valueOf(code));
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("c");
+        stringBuffer.append("a");
+        stringBuffer.append("t");
+
+        print("String buffer, creating cat.");
+        print(stringBuffer.toString());
+
+        String text2 = "DoggyBusiness";
+        int lText1 = text.length();
+        int lText2 = text2.length();
+
+        String ss = text2.substring(0, lText1);
+        if (ss.equals(text)) {
+            print("text1 IS substring of text2");
+        }
+    }
+
+
+    public int[] sort(int[] items) {
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items.length; j++) {
+                int curr = items[i];
+                int check = items[j];
+                if (curr <= check) {
+                    items[j] = curr;
+                    items[i] = check;
+                }
+            }
+        }
+        return items;
+    }
+
+    private void print(String text) {
+        System.out.println(text);
+    }
 }
--- java/src/test/java/warmup/WarmupTest.java
+++ java/src/test/java/warmup/WarmupTest.java
@@ -1,2 +1,24 @@
-package warmup;public class WarmupTest {
+package warmup;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class WarmupTest {
+
+    @Test
+    public void test() {
+        String animal = "Doggy";
+        Warmup warmup = new Warmup();
+        warmup.StringsPractice(animal);
+    }
+
+    @Test
+    public void sort() {
+        int[] items = new int[]{5, 8, 2, 1, 10};
+        int[] result = new Warmup().sort(items);
+        int[] expected = new int[]{1, 2, 5, 8, 10};
+        Assert.assertTrue(Arrays.equals(expected, result));
+    }
 }
