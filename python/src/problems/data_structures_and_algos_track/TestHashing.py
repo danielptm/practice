bc9589df91f1692d28dc9400453be419cca17426
Daniel Tuttle <danieltuttle@Daniels-MBP-2.hsd1.or.comcast.net>
2023-11-30 13:32:53 -0800

    made progress on hashing problem

--- python/src/problems/data_structures_and_algos_track/TestHashing.py
+++ python/src/problems/data_structures_and_algos_track/TestHashing.py
@@ -23,7 +23,6 @@ class TestHashing(unittest.TestCase):
         res = self.checkIfPangram(sentence)
         self.assertEqual(True, res)
 
-
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums) + 1
         for i in range(n):
@@ -32,7 +31,6 @@ class TestHashing(unittest.TestCase):
             else:
                 return i
 
-
     def test_misingNumber(self):
         nums = [3, 0, 1]
         e = 2
@@ -40,7 +38,7 @@ class TestHashing(unittest.TestCase):
         self.assertEqual(e, res)
 
     def test_missinNumber2(self):
-        nums = [0,1]
+        nums = [0, 1]
         e = 2
         res = self.missingNumber(nums)
         self.assertEqual(e, res)
@@ -60,7 +58,31 @@ class TestHashing(unittest.TestCase):
         self.assertEqual(e, res)
 
     def test_countElements2(self):
-        arr = [1,1,3,3,5,5,7,7]
+        arr = [1, 1, 3, 3, 5, 5, 7, 7]
         e = 0
         res = self.countElements(arr)
+        self.assertEqual(e, res)
+
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        lost_match_records = {}
+        result = [[], []]
+        winners = set()
+        for m in matches:
+            winners.add(m[0])
+            if m[1] not in lost_match_records.keys():
+                lost_match_records[1] = 1
+            else:
+                lost_match_records[1] = lost_match_records[1] + 1
+        for w in winners:
+            if w not in lost_match_records.keys():
+                result[0].append(w)
+        for k,v in lost_match_records.items():
+            if k == 1:
+                result[1].append(v)
+        return result
+
+    def test_findWinners(self):
+        matches = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]]
+        e = [[1, 2, 10], [4, 5, 7, 8]]
+        res = self.findWinners(matches)
         self.assertEqual(e, res)
