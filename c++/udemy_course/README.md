c3494a8592127c8305f594b17f51701184bc068c
Daniel Tuttle <disclosureguide@gmail.com>
2025-07-18 10:53:18 -0700

    completed through section 5 of udemy course

---
+++ c++/udemy_course/README.md
@@ -0,0 +1,2 @@
+This is practice code following along with the udemy course:
+https://www.udemy.com/course/beginning-c-plus-plus-programming
--- c++/udemy_course/main2.cpp
+++ c++/udemy_course/main2.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+/*
+ *You can use a namespace like this and then include or omit the namespace of the package the
+ *function comes from. See how std is used and not used in this file.
+ */
+using namespace std;
+
+/*
+ *This is a
+ *multiline
+ *comment
+ */
 int main() {
-    std::cout << "Hello world!";
-    return 0;
+    // single line comment
+    int favorite_number;
+    int num1;
+    int num2;
+    int num3;
+    int num4;
+    cout << "Enter your favoriet number between 1 and 100: "; // Standard outputstream to console. If you put a string like 1b2, it will read only the 1 because it's an int.
+    cin >> favorite_number; // Inputstream that defaults to keyboard
+    std::cout << "That's my favorite number too.... ";
+    //Create a new line after the text.
+    cout << "No really!, " << favorite_number << " is my favoriet number!" << std::endl;
+
+    cout << "************************************" << endl;
+    // here is a weird behavior if you try with input <1 2> (notice the space between 1 and 2). It will skip the second cin and print the correct output as if you filled in cin.
+    cout << "enter a number" << endl;
+    cin >> num1;
+    cout << "enter another number" << endl;
+    cin >> num2;
+    cout << "You entered " << num1 << " and " << num2 << std::endl;
+
+    cout << "************************************" << endl;
+
+    // Here is another weird one. If you enter invalid input in the first one it goes into error state, skips over the next cin and prints weird data.
+    cout << "enter 'b' (this is expecting an int)" << endl;
+    cin >> num3;
+    cout << "enter 1" << endl;
+    cin >> num4;
+    cout << "You entered " << num3 << " and " << num4 << std::endl;
 }
