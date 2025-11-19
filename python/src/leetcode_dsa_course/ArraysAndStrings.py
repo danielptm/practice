af9eefba26b751b7e95348c82dbdf0bcd63ed84b
Daniel <danielptm@me.com>
2025-11-18 18:49:57 -0800

    Added a solution for the maxAverageSubArray

--- python/src/leetcode_dsa_course/ArraysAndStrings.py
+++ python/src/leetcode_dsa_course/ArraysAndStrings.py
@@ -16,3 +16,28 @@ class ArraysAndStrings:
             res.append(n)
         res.sort()
         return res
+
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        max_avg = -100000000100000000100000000100000000
+        curr_sum = 0
+        left = right = 0
+        l = len(nums)
+        if l== 1:
+            return nums[0]
+        while right < l:
+            if (right - left) != k:
+                if (right - left) < k:
+                    curr_sum = curr_sum + nums[right]
+                    right = right + 1
+                elif (right - left) > k:
+                    curr_sum = curr_sum - nums[left]
+                    left = left + 1
+            if (right - left) == k:
+                avg_sum = curr_sum / k
+                if avg_sum > max_avg:
+                    max_avg = avg_sum
+                curr_sum = curr_sum - nums[left]
+                left = left + 1
+        return max_avg
+
+
--- python/src/leetcode_dsa_course/ArraysAndStringsTest.py
+++ python/src/leetcode_dsa_course/ArraysAndStringsTest.py
@@ -17,3 +17,21 @@ class ArraysAndStringsTest(unittest.TestCase):
         e = [0,1,9,16,100]
         r = aas.sortedSquares(i)
         self.assertEqual(e, r)
+
+    def test_findMaxAverage(self):
+        nums = [1, 12, -5, -6, 50, 3]
+        k = 4
+        output = 12.75000
+
+        aas = ArraysAndStrings()
+        res = aas.findMaxAverage(nums, k)
+        self.assertEqual(output, res)
+
+    def test_findMaxAverage2(self):
+        nums = [4,0,4,3,3]
+        k = 5
+        output = 2.80000
+
+        aas = ArraysAndStrings()
+        res = aas.findMaxAverage(nums, k)
+        self.assertEqual(output, res)
